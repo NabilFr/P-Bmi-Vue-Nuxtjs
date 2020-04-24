8b303963b21ad476663c36eeb433a2113766fecb
Nabil <benweb31@gmail.com>
2020-04-24 17:53:55 +0200

    ihm fonctionnel

---
+++ components/common/navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="shadow-lg bg-gray-800 py-6 px-4 flex items-center">
+    <div>
+      <svg
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        viewBox="0 0 24 24"
+        class="w-8 h-8"
+      >
+        <path d="M4 6h16M4 12h16M4 18h7" />
+      </svg>
+    </div>
+    <div class="text-center flex-1">
+      <p class="font-bold">BMI CALCULATOR</p>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <div classe="container">
-    <p>The home page</p>
+  <div class="container mx-auto pt-6 px-4 max-w-lg">
+    <gender />
+    <height />
+    <ageweight />
+    <button
+      class="bg-pink-600 font-bold uppercase p-4 fixed w-full bottom-0 left-0"
+    >Calculate your Bmi</button>
   </div>
 </template>
 
 <script>
-export default {};
+import gender from "~/components/home/gender";
+import height from "~/components/home/height";
+import ageweight from "~/components/home/ageWeight";
+export default {
+  components: {
+    gender,
+    height,
+    ageweight
+  }
+};
 </script>
